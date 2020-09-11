b0a74895579f73adf706ea7124507e7695040b7a
Jeff Pino <jppino.fie@unach.edu.ec>
2020-09-10 23:24:33 -0500

    Inicio

--- static/js/index.js
+++ static/js/index.js
@@ -2,7 +2,7 @@
 function hora(){
 	h = document.getElementById("hselec").value;
 	console.log(h);
-	message = new Paho.MQTT.Message(h)
+	message = new Paho.MQTT.Message(str(h))
 	message.destinationName="jeffersson.pino@gmail.com/Alimentador";
 	client.send(message);
 }
